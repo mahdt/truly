1e4a1e13e692bdaa97e1b4c48bd53eaca1cd89de
mahdt <mahdtauseef@gmail.com>
2015-06-15 03:21:24 -0400

    added code for post /number

--- router/index.js
+++ router/index.js
@@ -1,4 +1,4 @@
 module.exports = function (server) {
     server.use('/query', require('./routes/query'));
-    server.use('/post', require('./routes/number'));
+    server.use('/number', require('./routes/number'));
 };
--- router/routes/number.js
+++ router/routes/number.js
@@ -1,1 +1,41 @@
 //code for /number call goes here
+var server = require('../../server');
+var phoneList = server.phoneList;
+var express = require('express');
+var router = express.Router();
+
+
+router.post('/', function(req, res){
+	var phoneList = server.phoneList;
+	console.log("Handler for /number called");
+	console.dir(req.body);
+	var duplicateFlag = false;
+	var tempNumber = req.body.number;
+	tempNumber = '+1' +tempNumber.replace(/[-() ]+|\+1/g, '');
+	if (phoneList[tempNumber] == undefined) {
+  		phoneList[tempNumber] = [];
+	}
+  	else {
+  		console.log("size " +phoneList[tempNumber].length);
+  		for (var i = 0; i < phoneList[tempNumber].length; i++) {
+  			console.log("here");
+  			console.log(phoneList[tempNumber][i].context + " " + req.body.context);
+  			if (phoneList[tempNumber][i].context == req.body.context) {
+  				res.end("Already exists");
+  				duplicateFlag = true;
+  				break;
+  			}
+  		}
+	}
+	if (!duplicateFlag) {
+		phoneList[tempNumber].push({ name:'' + req.body.name + '', number:'' + tempNumber + '', context:'' + req.body.context+''});
+		console.log(phoneList[tempNumber]);
+		res.end("successful");
+	}
+});
+
+
+
+
+module.exports = router;
+
